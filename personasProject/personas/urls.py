78ccbfbee5e5a1dbf0f4c8265bc27a0757a8e107
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-06-26 18:18:11 -0500

    view de eliminar creada con post - templte de lista,formulario y eliminicación actualizados - urls configuradas, nuevo index

--- personasProject/personas/urls.py
+++ personasProject/personas/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.persona_formulario, name='persona_formulario'),
-        path('personaDatos/<int:k>/', views.persona_detalle, name='persona_detalle'),
+    path('', views.persona_lista, name='persona_lista'),
+    path('agregar/', views.persona_formulario, name='persona_agregar'),
+    path('eliminar/<int:pk>/', views.persona_confirmar_eliminar, name='persona_eliminar'),
+    path('detalle/<int:k>/', views.persona_detalle, name='persona_detalle'),
 ]
--- personasProject/personas/views.py
+++ personasProject/personas/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from .forms import PersonaForm
 from django.shortcuts import redirect
 
+def persona_lista(request):
+    personas = Persona.objects.all().order_by('id')
+    return render(request, 'lista.html', {'personas': personas})
+
 def persona_formulario(request):
     if request.method == 'POST':
         form = PersonaForm(request.POST)
         if form.is_valid():
-            persona = form.save()  # Aquí se guarda en la base de datos
-            return redirect('persona_detalle', k=persona.pk)  # Redirige al detalle
+            persona = form.save()
+            return redirect('persona_detalle', k=persona.pk)
     else:
         form = PersonaForm()
     
@@ -19,7 +23,6 @@ from .models import Persona
 def persona_detalle(request, k):
     persona = get_object_or_404(Persona, pk=k)
 
-    # Calcular el ID anterior y siguiente
     try:
         anterior = Persona.objects.filter(pk__lt=k).order_by('-pk')[0].pk
     except IndexError:
@@ -36,3 +39,15 @@ def persona_detalle(request, k):
         'siguiente': siguiente,
     }
     return render(request, 'detalle.html', contexto)
+
+def persona_confirmar_eliminar(request, pk):
+    persona = get_object_or_404(Persona, pk=pk)
+    
+    if request.method == 'POST':
+        if 'confirmar' in request.POST:
+            persona.delete()
+            return redirect('persona_lista')
+        else:
+            return redirect('persona_lista')
+    
+    return render(request, 'confirmar_eliminar.html', {'persona': persona})
