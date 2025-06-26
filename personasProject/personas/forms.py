3c3316ce3b03705a6dd7b097fcd99843c847a8ea
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-06-26 11:07:54 -0500

    Uso de Validation dentro de form - Validation error implementado y probado

--- personasProject/personas/forms.py
+++ personasProject/personas/forms.py
@@ -8,6 +8,12 @@ class PersonaForm(forms.ModelForm):
         widgets = {
             'nombres': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese nombres'}),
             'apellidos': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese apellidos'}),
-            'edad': forms.NumberInput(attrs={'class': 'form-control', 'min': 17}),
+            'edad': forms.NumberInput(attrs={'class': 'form-control', 'min': 0}),
             'donador': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
+
+    def clean_edad(self):
+        edad = self.cleaned_data.get('edad')
+        if edad < 18:
+            raise forms.ValidationError("Debes tener al menos 18 años.")
+        return edad
